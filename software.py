114026a1ef4b719b3d7d508e5bd558eae9b91f17
Matheus <Matheus.Cristo.Moraes@escola.pr.gov.br>
2025-02-19 12:10:52 -0300

    adicao

--- software.py
+++ software.py
@@ -1,6 +1,17 @@
  import random
 
- sorteio = random.randint(1,10)
- chute = input("Digite o seu chute:")
- print(sorteio)
- print(chute)
+print("@@@ JOGO DA ADIVINHAÇÃO @@@")
+print("Adivinhe o número que estou pensando, de 1 a 10")
+limite_tentativas = 3
+tentativa = 1
+while (limite_tentativas >= tentativa):
+    print("tentativa", tentativa)
+    tentativa = tentativa + 1
+chute = int(input("Digite o seu chute:", tentativa, ":"))
+print(sorteio)
+print(chute)
+
+if (chute == sorteio)
+    print("Parabéns, você acertou o número era", sorteio)
+if (chute != sorteio):
+    print("Você errou! O número era", sorteio)
