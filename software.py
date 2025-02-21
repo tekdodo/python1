9735085189580b389fbd28908d3a72b4c27d2e09
tekdodo <164498899+tekdodo@users.noreply.github.com>
2025-02-21 09:29:17 -0300

    Update software.py

--- software.py
+++ software.py
@@ -1,17 +1,25 @@
- import random
+import random
 
 print("@@@ JOGO DA ADIVINHAÇÃO @@@")
 print("Adivinhe o número que estou pensando, de 1 a 10")
-limite_tentativas = 3
+sorteio = random.randint(1,10)
+limite_tentativas = 30
 tentativa = 1
-while (limite_tentativas >= tentativa):
+while (limite_tentativas >= tentativa): # enquanto (ver)
     print("tentativa", tentativa)
-    tentativa = tentativa + 1
-chute = int(input("Digite o seu chute:", tentativa, ":"))
-print(sorteio)
-print(chute)
+   
+    chute = int(input("Digite o seu chute:"))
 
-if (chute == sorteio)
-    print("Parabéns, você acertou o número era", sorteio)
-if (chute != sorteio):
-    print("Você errou! O número era", sorteio)
+    #se (condição)
+    if (chute == sorteio):
+        print("Parabéns, você acertou o número era", sorteio)
+        break
+    elif (chute > sorteio):
+        print("Chute um numero menor!")
+        break
+    elif (chute < sorteio):
+        print("Chute um numero maior")
+    
+    # while (ENQUANTO) VERDADEIRO: EXECUTA OS CODIGOS / FALSO: PARA DE EXECUTAR OS CODIGOS
+   
+    tentativa = tentativa + 1
