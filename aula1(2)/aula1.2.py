73658be87e6c6fdfe94284724e1721e1a58e479e
tekdodo <164498899+tekdodo@users.noreply.github.com>
2025-05-16 09:38:06 -0300

    Add files via upload

---
+++ aula1(2)/aula1.2.py
@@ -0,0 +1,13 @@
+def cria_conta(agencia, numero, titular, saldo, limite, senha):
+    conta = {
+        "agencia": agencia,
+        "numero": numero,
+        "titular": titular,
+        "saldo": saldo,
+        "limite": limite,
+        "senha": senha,
+    }
+    return conta 
+
+conta2 = cria_conta(123, "2000-2", "Ciclano", 200.0, 500.0, 1234)
+print(f"Agência: {conta2 ['agencia']}, Conta: {conta2['numero']}, - SALDO: {conta2['saldo']}")
