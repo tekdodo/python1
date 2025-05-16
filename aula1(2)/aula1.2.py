90d3a92e338f69abf94fa794499ed43574beec57
tekdodo <164498899+tekdodo@users.noreply.github.com>
2025-05-16 11:05:29 -0300

    Update aula1.2.py

--- aula1(2)/aula1.2.py
+++ aula1(2)/aula1.2.py
@@ -11,3 +11,10 @@ def cria_conta(agencia, numero, titular, saldo, limite, senha):
 
 conta2 = cria_conta(123, "2000-2", "Ciclano", 200.0, 500.0, 1234)
 print(f"Agência: {conta2 ['agencia']}, Conta: {conta2['numero']}, - SALDO: {conta2['saldo']}")
+
+def extrato(conta):
+    return conta['saldo']
+
+def sacar(conta, valor):
+    if conta['saldo'] > valor:
+        conta ['saldo'] -=+
